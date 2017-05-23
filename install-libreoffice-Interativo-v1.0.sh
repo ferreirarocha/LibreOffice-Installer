c43b3721497505c2ae5a1aad16b992cfe01cd13c
ferreirarocha <alfabech@outlook.com>
2017-05-23 09:10:45 -0300

    Update install-libreoffice-Interativo-v1.0.sh

--- install-libreoffice-Interativo-v1.0.sh
+++ install-libreoffice-Interativo-v1.0.sh
@@ -167,7 +167,8 @@ case $? in
                                                 sudo -S zypper -n install notify-send
 
                                             elif [ -e /usr/bin/dpkg  ]; then
-                                                sudo -S  apt-get install curl notify-send -y
+                                                sudo -S  apt-get install curl libnotify-bin -y
+
 
                                             else
                                                 echo "gernciador não encontrado"  
